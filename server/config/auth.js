711249a2847a8be38c6ca71837087f76447051ac
Luke Platz <lplatz@umich.edu>
2017-02-02 14:19:32 -0600

    add real callback

--- server/config/auth.js
+++ server/config/auth.js
@@ -3,6 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1870619553215877', // your App ID
         'clientSecret'  : 'f080bf984d55be274c4fa3faf9b56afe', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        // 'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
+        'callbackURL'   : 'https://clue.mybluemix.net/auth/facebook/callback'
     }
 };

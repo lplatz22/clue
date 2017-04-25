775fdfdc6bd51bd2aba9546794caa449154e85bd
Luke Platz <=>
2017-04-24 22:29:37 -0500

    game changes, version bump

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  version = "0.0.1";
+  version = "1.0.0";
   title = 'app works!';
 }
